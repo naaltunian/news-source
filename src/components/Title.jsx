6b2e18edbf207bf3bfa5f84a1743456a8bf99c13
Robert B Garcia <robgarcia552@gmail.com>
2018-09-16 12:41:16 -0400

    title component

---
+++ src/components/Title.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Title extends Component {
+    render() {
+      return (
+        <div >
+  
+        </div>
+      );
+    }
+  }
+  
+  export default Title;
