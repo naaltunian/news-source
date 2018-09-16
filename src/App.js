c54cec61afb90e0b7824899a0e5076d202b090ab
naaltunian <naaltunian@icloud.com>
2018-09-16 13:09:07 -0400

    create get request

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+
+const key = ``
 
 class App extends Component {
+
+  newsRequest = () => {
+    axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${key}`)
+    .then((res) => {
+      console.log(res);
+    });
+  }
+
   render() {
     return (
       <div >
+
+        <h1>Test</h1>
 
       </div>
     );
