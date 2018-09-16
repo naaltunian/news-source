a1fa9b5ad79f44f96e0adecedbf5a375a13c9156
Robert B Garcia <robgarcia552@gmail.com>
2018-09-16 13:22:51 -0400

    radio button import to Category Filter

--- src/components/CategoryFilter.jsx
+++ src/components/CategoryFilter.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Radio } from 'semantic-ui-react'
 
 class CategoryFilter extends Component {
     render() {
       return (
         <div >
             <h1>Categories:</h1>
+            
         </div>
       );
     }
