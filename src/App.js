be23f60215513c1f3b46da27bb5d2843519633f5
Robert B Garcia <robgarcia552@gmail.com>
2018-09-16 12:46:44 -0400

    display article component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Title from './components/Title';
+import DisplayArticle from './components/DisplayArticle';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div >
         <Title/>
+        <DisplayArticle/>
       </div>
     );
   }
