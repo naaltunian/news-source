4c02082c605a0d11c5b11d45b43d10f9fd5f4ea5
naaltunian <naaltunian@icloud.com>
2018-09-17 14:39:35 -0400

    add react-router-dom and home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import Title from './components/Title';
-import Display from './components/Display';
 import Navbar from './components/Navbar';
 import CategoryFilter from './components/CategoryFilter';
 import './App.css';
 import axios from 'axios';
+import News from './components/News';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 let keys = require('./config.js')
 
@@ -22,16 +23,22 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <Navbar/>
-        <Title/>
-        <CategoryFilter/>
-        <Display 
-        articles={this.state.articles}
-        />
-      </div>
+      <Router>
+        <div>
+          <Navbar/>
+          <Route />
+          <Route />
+          <Route />
+        </div>
+      </Router>
     );
   }
 }
 
 export default App;
+
+// <Title/>
+// <CategoryFilter/>
+// <News
+// articles={this.state.articles}
+// />
