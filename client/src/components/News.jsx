43bc19f569d81fd0dc5653816b52bb087e6c079c
Robert B Garcia <robgarcia552@gmail.com>
2018-09-18 13:32:58 -0400

    attributes display for articles

--- client/src/components/News.jsx
+++ client/src/components/News.jsx
@@ -26,7 +26,15 @@ class Display extends React.Component {
         <CategoryFilter />
         <ul>
           {this.state.articles.map(article =>  {
-            return <li key={article.title}><a href={article.url} >{article.title}</a></li>})
+            return(
+                <div>
+                    <img key={article.title} src={article.urlToImage} alt={article.title} width='300px' height='200px'/>
+                    <li key={article.title}><a href={article.url} >{article.title}</a></li>
+                    <li key={article.title}>{article.author}</li>
+                    <li key={article.title}>{article.description}</li>
+                </div>
+            )
+          })
           }
         </ul>
       </div>
