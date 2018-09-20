0a79e0b82f6e8dc60579dad1b2176ca448988949
naaltunian <naaltunian@icloud.com>
2018-09-20 09:17:32 -0400

    map over all articles and return DisplayArticle component

--- client/src/components/DisplayArticle.jsx
+++ client/src/components/DisplayArticle.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const DisplayArticle = () => {
+const DisplayArticle = (props) => {
     return (
       <div>
-        <h1>Display Article Component</h1>
+          {props.article.urlToImage && <img key={props.article.title} src={props.article.urlToImage} alt={props.article.title} width='300px' height='200px'/>}
+          {props.article.title && <h4 key={props.article.title}><a href={props.article.url} >{props.article.title}</a></h4>}
+          {props.article.author && <p key={props.article.title}>By: {props.article.author}</p>}
+          {props.article.description && <p key={props.article.title}>{props.article.description}</p>}
       </div>
     );
   }
