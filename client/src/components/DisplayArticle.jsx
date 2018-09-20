43fdae0d88700771a8f46ca2be551bfa20f6f7bc
Robert B Garcia <robgarcia552@gmail.com>
2018-09-20 12:27:00 -0400

    images have default and link opens new tab

--- client/src/components/DisplayArticle.jsx
+++ client/src/components/DisplayArticle.jsx
@@ -18,8 +18,8 @@ const DisplayArticle = (props) => {
           <Grid.Column >
             <Grid.Row >
               <Card style={cards}>
-    {props.article.urlToImage ? <Image key={props.article.title} src={props.article.urlToImage} width='300px' height='200px'/> : <Image key={props.article.title} src='https://images.unsplash.com/photo-1529243856184-fd5465488984?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=95834c9e01a9ff2a5a61c79fc92a180f&auto=format&fit=crop&w=1969&q=80' width='300px' height='200px'/> }
-                {props.article.title && <h4 key={props.article.title}><a href={props.article.url} >{props.article.title}</a></h4>}
+    {props.article.urlToImage ? <Image key={props.article.title} src={props.article.urlToImage} width='300px' height='200px'/> : <Image key={props.article.title} src='https://images.unsplash.com/photo-1523995462485-3d171b5c8fa9?ixlib=rb-0.3.5&s=58c415f3926f83db083eff8967fac94b&auto=format&fit=crop&w=975&q=80' width='300px' height='200px'/> }
+                {props.article.title && <h4 key={props.article.title}><a target='to_blank'href={props.article.url} >{props.article.title}</a></h4>}
                 {props.article.author && <p key={props.article.title}>By: {props.article.author}</p>}
                 {props.article.description && <p key={props.article.title}>{props.article.description}</p>}
               </Card>
