5c1364fccec3d1875a75ae9e0c833ba26d4093c8
naaltunian <naaltunian@icloud.com>
2018-09-25 13:49:09 -0400

    remove extra all button from categoryFilter

--- client/src/components/CategoryFilter.jsx
+++ client/src/components/CategoryFilter.jsx
@@ -8,7 +8,7 @@ const words = {
 }
 
 class CategoryFilter extends Component {
-    
+
     render() {
 
       return (
@@ -16,15 +16,12 @@ class CategoryFilter extends Component {
             <Container textAlign='center'><h2 style={words}>Select Category</h2></Container>
             <Container textAlign="center">
               <Button.Group >
-
-                <Button onClick={this.props.handleChange} value='all'>All</Button>
                 <Button onClick={this.props.handleChange} value='general'>General</Button>
                 <Button onClick={this.props.handleChange} value='business'>Business</Button>
                 <Button onClick={this.props.handleChange} value='entertainment'>Entertainment</Button>
                 <Button onClick={this.props.handleChange} value='health'>Health</Button>
                 <Button onClick={this.props.handleChange} value='science'>Science</Button>
                 <Button onClick={this.props.handleChange} value='technology'>Technology</Button>
-
               </Button.Group>
             </Container>
 
